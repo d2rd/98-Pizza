acb6844337a722d634f9aace688cfef7a9049cbf
John Ware <johnware@Johns-MacBook-Air.local>
2017-05-19 14:26:35 -0700

    final changes for project

---
+++ frontend_ui/app.js
@@ -0,0 +1,64 @@
+function specialItems() {
+  this.specialItems;
+  document.getElementById("special-items").innerHTML = "";
+}
+
+function menuItems() {
+  this.menuItems;
+  document.getElementById("menu-items").innerHTML = "";
+}
+
+function orderHistory() {
+  this.orderHistory;
+  document.getElementById("order-history").innerHTML = "";
+}
+
+function fName() {
+  this.fName;
+  document.getElementByName("fname").innerHTML = "";
+}
+
+function lName() {
+  this.lName;
+  document.getElementByName("lname").innerHTML = "";
+}
+
+function email() {
+  this.email;
+  document.getElementByName("email").innerHTML = "";
+}
+
+function password() {
+  this.password;
+  document.getElementByName("password").innerHTML = "";
+}
+
+function confirmPassword() {
+  this.password;
+  document.getElementByName("confirm-password").innerHTML = "";
+}
+
+function streetAddress1() {
+  this.streetAddress1;
+  document.getElementByName("address1").innerHTML = "";
+}
+
+function streetAddress2() {
+  this.streetAddress2;
+  document.getElementByName("address2").innerHTML = "";
+}
+
+function city() {
+  this.city;
+  document.getElementByName("city").innerHTML = "";
+}
+
+function state() {
+  this.state;
+  document.getElementByName("state").innerHTML = "";
+}
+
+function zipcode() {
+  this.zipcode;
+  document.getElementByName("zipcode").innerHTML = "";
+}
